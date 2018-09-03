1e47a8a87cf8b1e068c01fae352e096a7fc05979
ohiofi <20hz20khz@gmail.com>
2018-09-03 18:35:16 -0400

    added printf comparisons

--- MoneyFormatting.java
+++ MoneyFormatting.java
@@ -4,9 +4,21 @@ public class MoneyFormatting
 {
 	public static void main(String[] args)
 	{
-		double foobar = -00000000324.30000000000000003;
-		DecimalFormat money = new DecimalFormat("$0.00");
+		double foobar = 00000000324.30000000000000003;
+		System.out.printf("$%10.2f\n",foobar);
+		//Will output "$  -324.30"
+		DecimalFormat money = new DecimalFormat("$.00");
 		System.out.println(money.format(foobar));
-		//Will print foobar as "-$324.30"}}
+		//Will output "-$324.30"
+		
+		foobar = -00000000324.30000000000000003;
+		System.out.printf("$%10.2f\n",foobar);
+		//Will output "$  -324.30"
+		System.out.println(money.format(foobar));
+		//Will output "-$324.30"
+		
+		//You CAN combine printf and DecimalFormat
+		System.out.printf("%10s\n",money.format(foobar));
+		
 	}
 }
