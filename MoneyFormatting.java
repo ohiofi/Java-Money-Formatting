37394bcd4d85b39f2a03a428b2ab69fd24f653ac
Justin Riley <20hz20khz@gmail.com>
2018-09-10 11:55:12 -0400

    fixed outputs

--- MoneyFormatting.java
+++ MoneyFormatting.java
@@ -6,19 +6,23 @@ public class MoneyFormatting
 	{
 		double foobar = 00000000324.30000000000000003;
 		System.out.printf("$%10.2f\n",foobar);
-		//Will output "$  -324.30"
+		//Will output "$    324.30"
+
+		//Create a decimal format called money
 		DecimalFormat money = new DecimalFormat("$.00");
 		System.out.println(money.format(foobar));
+		//Will output "$324.30"
+
+		double negativeFoobar = -00000000324.30000000000000003;
+		System.out.printf("$%10.2f\n",negativeFoobar);
+		//Will output "$   -324.30"
+		System.out.println(money.format(negativeFoobar));
 		//Will output "-$324.30"
-		
-		foobar = -00000000324.30000000000000003;
-		System.out.printf("$%10.2f\n",foobar);
-		//Will output "$  -324.30"
-		System.out.println(money.format(foobar));
-		//Will output "-$324.30"
-		
-		//You CAN combine printf and DecimalFormat
+
+		//You CAN combine printf AND DecimalFormat
 		System.out.printf("%10s\n",money.format(foobar));
-		
+		//Will output "   $324.30"
+		System.out.printf("%10s\n",money.format(negativeFoobar));
+		//Will output "  -$324.30"
 	}
 }
